03144a97c4933da35f8dca1269e88183f11643a3
CristhTejada <42393415+CristhTejada@users.noreply.github.com>
2021-11-19 17:02:13 -0600

    Update web scraping genre violence El Salvador.py

--- scripts/web scraping genre violence El Salvador.py
+++ scripts/web scraping genre violence El Salvador.py
@@ -10,13 +10,13 @@ import itertools
 
 # Rules and keywords definition for El Salvador coordinates
 
-
 loc = '13.794185, -88.89653, 120km'
 df = pd.DataFrame(itertools.islice(sntwitter.TwitterSearchScraper('keywords since:2020-01-01 until:2020-12-31 geocode:"{}"'.format(loc)).get_items(), 31000))[['user', 'content', 'date', 'coordinates']]
 df['user_location'] =  df['user'].apply(lambda x: x['location'])
 
 
 # Process to anonymize users
+
 del df["user"]
 df['User'] = 'Constant Value'
 df['User'] = '@anonymized_user'
@@ -30,6 +30,8 @@ df['content'] = df['content'].str.replace('\n', ' ')
 # Save to csv file
 
 df.to_csv("tweetsviolencia4.csv")
+
+#By C.tejada
 
 
 
